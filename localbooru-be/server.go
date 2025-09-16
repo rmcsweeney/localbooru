1cdb61ad37bebb589ced78c6258756fe7d87e26e
rmcsweeney <rmcsweeney@utexas.edu>
2025-09-16 11:03:00 -0500

    Basic file sending

--- localbooru-be/server.go
+++ localbooru-be/server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 )
 
 var repository *RepositoryImpl
@@ -15,6 +18,7 @@ func main() {
 	mux.HandleFunc("/hello", getHello)
 	mux.HandleFunc("/posts", getPosts)
 	mux.HandleFunc("/posts/{id}", getPostById)
+	mux.HandleFunc("/media/{type}/{file}", getMedia)
 	http.ListenAndServe(":8080", mux)
 	println("Server started")
 }
@@ -29,6 +33,29 @@ func getRoutes() {
 
 func getPosts(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func getMedia(w http.ResponseWriter, r *http.Request) {
+	mediaType := r.PathValue("type")
+	fileName := r.PathValue("file")
+	//fmt.Fprintf(w, "Media type: %s, file: %s", mediaType, fileName)
+	cwd, _ := os.Getwd()
+	file, err := os.Open(cwd + "/assets/" + mediaType + "/" + fileName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+	_, extension, _ := strings.Cut(fileName, ".")
+
+	w.Header().Set("Content-Type", mediaType+"/"+extension)
+	buffer := new(bytes.Buffer)
+	_, _ = buffer.ReadFrom(file)
+	w.Write(buffer.Bytes())
+	w.Header().Set("Content-Disposition", "inline; filename="+fileName+"")
+	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+
+	r.Close = true
 }
 
 func getPostById(w http.ResponseWriter, r *http.Request) {
