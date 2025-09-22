723369a12f670766886c6fde325d611bbde66ec7
rmcsweeney <rmcsweeney@utexas.edu>
2025-09-21 20:55:58 -0500

    moar layout

--- localbooru/pages/posts/index.js
+++ localbooru/pages/posts/index.js
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import {useEffect, useState} from "react";
 import Link from "next/link";
+import ThemeSwitcher from "../../components/buttons/themeswitcher";
 
 export default function Post() {
 
 
     const [postData, setPostData] = useState([]);
 
+    //TODO: figure out how to move the below & similar to a config/env file?
     const baseUrl = 'http://localhost:8080/'
     
     // Images to load per click
@@ -26,25 +28,29 @@ export default function Post() {
         }
     };
 
+    //Calls fetchPost to populate the page at load time once (empty deps array).
     useEffect(() => {
         fetchPost();
     }, []);
 
 
     return <>
-        <Image src="/images/Al_Sneed.png" alt={"Sneedem Feedem!"} width={100} height={100} className={"justify-center"} />
-        <p className={"text-orange-700 dark:text-emerald-200"} >
-            Welcome to Localbooru!
-        </p>
+        <div className={"grid grid-cols-[10%_90%] dark:bg-slate-800"}>
+            <Image src="/images/Al_Sneed.png" alt={"Sneedem Feedem!"} width={100} height={100} className={"justify-center"} />
+            <p className={"text-orange-700 dark:text-emerald-200"} >
+                Welcome to Localbooru!
+            </p>
+        </div>
         <div className={"grid grid-cols-[20%_80%]"}>
             <div className={"m-1"}>
                 <button className={"border-4 border-b-cyan-700 mb-1"} onClick={fetchPost}> Click to load {loadSize} image{loadSize === 1 ? "" : "s"}</button>
-                <input defaultValue={"Enter tags..."}></input>
+                <input className={"max-w-[100%]"} defaultValue={"Enter tags..."}></input>
             </div>
             <div>
                 <div className={"grid-cols-4 grid"} >
                     {postData.map( (post, index) => {
-                        return <Link href={"/posts/" + post?.ID} key={post?.ID}>
+                        return <Link href={"/posts/" + post?.ID} key={post?.ID}
+                        className={"m-1 rounded"}>
                             <Image className={"flex-auto"} width={300} height={300} src={baseUrl + "assets/images" + "/" + post?.FileName + "." + post?.FileType} key={index} alt={post?.FileName} />
                         </Link>
                     })}
@@ -53,6 +59,6 @@ export default function Post() {
 
         </div>
 
-
+        <ThemeSwitcher></ThemeSwitcher>
     </>;
 }
