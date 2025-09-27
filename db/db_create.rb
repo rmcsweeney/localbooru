52388c2719870fc327eeadb066234da95127886f
rmcsweeney <rmcsweeney@utexas.edu>
2025-09-26 21:41:15 -0500

    DB table script

---
+++ db/db_create.rb
@@ -0,0 +1,32 @@
+require 'sqlite3'
+
+db = SQLite3::Database.new('lcbru.db')
+
+db.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        filename TEXT,
+        filetype TEXT,
+        created_at TEXT
+    );
+SQL
+
+db.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS tags (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        count INTEGER
+    );
+SQL
+
+db.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS post_tags (
+        post_id INTEGER,
+        tag_id INTEGER,
+        PRIMARY KEY (post_id, tag_id),
+        FOREIGN KEY (post_id) REFERENCES posts(id),
+        FOREIGN KEY (tag_id) REFERENCES tags(id)
+    );
+SQL
+
+db.close
